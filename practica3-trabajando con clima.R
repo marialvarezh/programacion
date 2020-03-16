1d7815b89288f025e5992c6d7319f48953f29fdd
marialvarezh <mc.alvarez@edu.uah.es>
2020-03-16 15:19:14 +0100

    cambios mery

--- practica3-trabajando con clima.R
+++ practica3-trabajando con clima.R
@@ -1,1 +1,52 @@
 getwd()
+##practica 3- trabajando con datos climaticos 
+##Autores: Daniel García Betancort y María Carmen Álvarez Hernández 
+##Fecha: 17/03/2020 
+
+#establecemos el directorio de trabajo
+getwd()
+setwd()
+
+##cargamos las librerias necesarias que esten instaladas previmente con install.packges()
+library(sp)
+library(raster)
+library(envirem)
+
+##Descargamos los datos de clima desde la pagina Chelsa 
+##escenario de 2014-2016; meses julio y agosto; temperatura maxima
+
+##para abril el archivo raster con los datos temperaturas julio y agosto 
+TempJulio <- raster("datosclima\\CHELSA_tasmax_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc_7_2041-2060_V1.2.tif") 
+TempAgosto <- raster("datosclima\\CHELSA_tasmax_mon_ACCESS1-0_rcp85_r1i1p1_g025.nc_8_2041-2060_V1.2.tif")
+
+
+class(TempJulio) 
+class(TempAgosto)
+
+extent(TempJulio)##extent da las coordenadas minimas y las maximas
+extent(TempAgosto)
+
+ncell(TempJulio)#ncell da el numero total de celdas
+ncell(TempAgosto)
+
+crs(TempJulio)#crs para ver el sistema de coordenadas 
+crs(TempAgosto)
+
+plot(TempJulio)
+
+##hacemos un clip de la zona de estudio con la funcion crop
+tenerife <- as(extent(-16.78,-16.40,28,28.35), 'SpatialPolygons')##las coordenadas de la zona que quiero recortar 
+recorte <- crop(TempJulio, tenerife) ##crop para recortar el raster 
+
+plot(recorte) ##para visualizar la capa raster
+
+writeRaster(recorte, filename="recorte.tif", format = "GTiff", overwrite=TRUE)
+
+temperatura<-  hist(recorte, col= "red") ##visualizamos los datos en un histograma 
+
+View(recorte)
+src(recorte)
+
+summary(recorte)
+class(recorte)
+ddddd
